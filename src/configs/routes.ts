b9d3e84cc6520d27170db999f984e78a8aa3181d
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-08-19 00:17:10 +0800

    chore: bypass auth middleware for public routes

---
+++ src/configs/routes.ts
@@ -0,0 +1,8 @@
+import { Route } from "@/types/route";
+
+export const publicRoutes: Route[] = [
+  {
+    path: "/v1/auth/login",
+    method: "POST",
+  }
+];
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,17 @@
 import { jwt } from "hono/jwt";
 import { createMiddleware } from "hono/factory";
 import { JWT_SECRET } from "@/configs/constant";
+import { publicRoutes } from "@/configs/routes";
 
 export const auth = createMiddleware(async (c, next) => {
+  const isPublicRoute = publicRoutes.some(
+    (route) => route.path === c.req.path && route.method === c.req.method
+  );
+
+  if (isPublicRoute) {
+    return next();
+  }
+
   const jwtMiddleware = jwt({
     secret: JWT_SECRET,
   });
