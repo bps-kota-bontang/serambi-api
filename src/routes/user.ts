647bb6e655ff3e1056be0ab76ff834231369d09a
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-08-21 09:50:43 +0800

    feat: add api users me

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,21 @@
 import { getUser, getUsers } from "@/services/user";
+import { JWT } from "@/types/jwt";
 import { Hono } from "hono";
 
 const app = new Hono();
+
+app.get("/me", async (c) => {
+  const claims = c.get("jwtPayload") as JWT;
+  const result = await getUser(claims.sub);
+
+  return c.json(
+    {
+      data: result.data,
+      message: result.message,
+    },
+    result.code
+  );
+});
 
 app.get("/", async (c) => {
   const result = await getUsers();
