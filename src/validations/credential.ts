bbed6a5ef1f5d9a84adb08ff9898df212bd547a4
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-08-22 23:32:02 +0800

    chore: change create credential validation to nullish

--- src/validations/credential.ts
+++ src/validations/credential.ts
@@ -2,10 +2,10 @@ import { password } from "bun";
 import { z } from "zod";
 
 export const CreateCredentialSchema = z.object({
-  username: z.string().nullable(),
-  password: z.string().nullable(),
+  username: z.string().nullish(),
+  password: z.string().nullish(),
   hasSso: z.boolean(),
-  note: z.string().nullable(),
+  note: z.string().nullish(),
 });
 
 export type CreateCredentialPayload = z.infer<typeof CreateCredentialSchema>;
